24ea62608b19213417aeebd104b85fd1daabd917
Thomas Körner <t.koerner@heimrich-hannot.de>
2021-07-14 10:57:15 +0200

    refactored database access to Connection class

--- src/DataContainer/VersionsContainer.php
+++ src/DataContainer/VersionsContainer.php
@@ -12,9 +12,11 @@
 namespace HeimrichHannot\VersionsBundle\DataContainer;
 
 
+use Contao\Automator;
 use Contao\Config;
 use Contao\CoreBundle\Monolog\ContaoContext;
-use Contao\Database;
+use Contao\System;
+use Doctrine\DBAL\Connection;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 
@@ -22,6 +24,10 @@ class VersionsContainer
 {
     /** @var array */
     protected $bundleConfig = [];
+    /**
+     * @var Connection
+     */
+    protected $connection;
 
     /** @var LoggerInterface */
     private $logger;
@@ -31,42 +37,50 @@ class VersionsContainer
      * @param array $bundleConfig
      * @param LoggerInterface $logger
      */
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, array $bundleConfig, Connection $connection)
     {
-//        $this->bundleConfig = $bundleConfig;
+        $this->bundleConfig = $bundleConfig;
         $this->logger       = $logger;
+        $this->connection = $connection;
     }
 
+    /**
+     * Clean the version table respecting persistent table configuration
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\DBAL\Exception
+     */
     public function cleanTable(): void
     {
-        $objDatabase = Database::getInstance();
-
         if ($this->hasPersistentTables()) {
             // Fallback for legacy integrations, will be default 0 in version 3.
             $period = $this->bundleConfig['persistent_version_period'] ?? (int)Config::get('versionPeriod');
             $tstamp = $period > 0 ?  time() - (int)Config::get('versionPeriod') : 0;
-            $objDatabase->query("DELETE FROM tl_version WHERE tstamp<$tstamp AND fromTable NOT IN('" . implode('\',\'', $GLOBALS['PERSISTENT_VERSION_TABLES']) . "')");
+            $this->connection->executeQuery("DELETE FROM tl_version WHERE tstamp<$tstamp AND fromTable NOT IN('" . implode('\',\'', $GLOBALS['PERSISTENT_VERSION_TABLES']) . "')");
         } else {
             // Truncate the table
             // Delete old versions from the database
             $tstamp = time() - (int)Config::get('versionPeriod');
-            $objDatabase->query("DELETE FROM tl_version WHERE tstamp<$tstamp");
+            $this->connection->executeQuery("DELETE FROM tl_version WHERE tstamp<$tstamp");
         }
-
     }
 
+    /**
+     * Purge the version table respecting persistent table configuration
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\DBAL\Exception
+     */
     public function purgeTable(): void
     {
-        $objDatabase = Database::getInstance();
-
         if ($this->hasPersistentTables()) {
-            $objDatabase->execute("DELETE FROM tl_version WHERE fromTable NOT IN('" . implode('\',\'', $GLOBALS['PERSISTENT_VERSION_TABLES']) . "')");
+            $this->connection->executeQuery("DELETE FROM tl_version WHERE fromTable NOT IN('" . implode('\',\'', $GLOBALS['PERSISTENT_VERSION_TABLES']) . "')");
 
             $this->log('Cleared non persistent entries from version table', __METHOD__, TL_CRON);
         } else {
-            $objDatabase->execute("TRUNCATE TABLE tl_version");
-
-            $this->log('Purged the version table', __METHOD__, TL_CRON);
+            /** @var Automator $automator */
+            $automator = System::importStatic(Automator::class);
+            $automator->purgeVersionTable();
         }
     }
 
